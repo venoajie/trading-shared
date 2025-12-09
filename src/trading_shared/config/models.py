3705ab5eabc80a5b4b03329233b61fc4786a7651
venoajie <59521823+venoajie@users.noreply.github.com>
2025-12-10 05:50:18 +0700

    add SecretStr

--- src/trading_shared/config/models.py
+++ src/trading_shared/config/models.py
@@ -4,18 +4,22 @@
 from typing import Optional
 
 # --- Installed  ---
-from pydantic import BaseModel, Field, computed_field
-
+from pydantic import BaseModel, Field, computed_field, SecretStr
+from urllib.parse import quote_plus
 
 class RedisSettings(BaseModel):
     url: str
     db: int
-    password: Optional[str] = None
+    password: Optional[SecretStr] = None
+    write_concurrency_limit: int = Field(
+        default=4,
+        description="Concurrency limit for bulk write operations to Redis."
+    )
 
 
 class PostgresSettings(BaseModel):
     user: str
-    password: str
+    password: SecretStr
     host: str
     port: int
     db: str
@@ -23,14 +27,17 @@ class PostgresSettings(BaseModel):
     @computed_field
     @property
     def dsn(self) -> str:
-        """Computes the DSN string for asyncpg."""
-        return f"postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.db}"
+        """Computes the DSN string for asyncpg."""       
+        encoded_user = quote_plus(self.user)
+        encoded_password = quote_plus(self.password.get_secret_value())
+        return f"postgresql://{encoded_user}:{encoded_password}@{self.host}:{self.port}/{self.db}"
+        #return f"postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.db}"
 
 
 class ExchangeSettings(BaseModel):
     # API keys are now optional, allowing this model to be used
     # for both public and private clients.
-    client_id: Optional[str] = None
+    client_id: Optional[SecretStr] = None
     client_secret: Optional[str] = None
 
     # WebSocket and REST URLs
@@ -39,5 +46,5 @@ class ExchangeSettings(BaseModel):
 
 
 class TelegramSettings(BaseModel):
-    bot_token: str
+    bot_token: SecretStr
     chat_id: str
