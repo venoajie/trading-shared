9c9b98a70f14ae4d182287367174c99a9c534c32
venoajie <venoajie@googlemail.com>
2025-12-10 18:27:54 +0700

    fixing import error-add constant

---
+++ src/trading_shared/utils/constants.py
@@ -0,0 +1,8 @@
+# src/trading_shared/utils/constants.py
+
+class ExchangeNames:
+    """
+    Centralized, canonical string names for supported exchanges.
+    """
+    DERIBIT = "deribit"
+    BINANCE = "binance"
